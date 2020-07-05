b6edd69136f1fd61c0c21a6207891d24a71a69e8
unknown <mdeniro6@gmail.com>
2020-07-05 16:50:32 +0100

    react hooks tutorial for intern

---
+++ precache-manifest.d184c8273e13da4194d4e14e269f1303.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ce11ff89711a9a2039db72633fb83a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3e36635c9f1c9d753a",
+    "url": "/static/css/main.530ef7aa.chunk.css"
+  },
+  {
+    "revision": "a814544eefa2eef1cfcd",
+    "url": "/static/js/2.3d85c6d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d85c6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3e36635c9f1c9d753a",
+    "url": "/static/js/main.4a5bbefc.chunk.js"
+  },
+  {
+    "revision": "7ec9d38344d415a52b7e",
+    "url": "/static/js/runtime-main.ecbd66d2.js"
+  },
+  {
+    "revision": "5a62823dbf118ae8ba4530be72acf317",
+    "url": "/static/media/bb.5a62823d.gif"
+  },
+  {
+    "revision": "0bcd43ba5b850a7dd635615ce6424a14",
+    "url": "/static/media/bbadicon.0bcd43ba.png"
+  },
+  {
+    "revision": "4ba0f6db86ff8115042db1bd87868b77",
+    "url": "/static/media/bbadintro.4ba0f6db.gif"
+  }
+]);
